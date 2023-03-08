7230950f87ea6edd7c1371780214677a46131cd5
Tim Smith <tim.smith2814@gmail.com>
2023-03-08 10:55:35 +0000

    Lab 1 tasks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [location, setLocation] = useState({});
+  const [mapUrl, setMapUrl] = useState("");
+  const [showError, setShowError] = useState(false);
+
+  // change the value of searchQuery whenever I type in the input
+  function handleChange(event) {
+    setSearchQuery(event.target.value);
+  }
+
+  async function handleSearch() {
+    try {
+      // get the location data
+      const API = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${searchQuery}&format=json`;
+      const res = await axios.get(API);
+
+      setLocation(res.data[0]);
+
+      // change the url for the map image
+      const lat = res.data[0].lat;
+      const lon = res.data[0].lon;
+      setMapUrl(
+        `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_KEY}&center=${lat},${lon}&zoom=18`
+      );
+    } catch (error) {
+      console.log(error);
+      setLocation({});
+      setMapUrl("");
+      setShowError(true);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onChange={handleChange} />
+      <button onClick={handleSearch}>Explore!</button>
+      {showError && <p>That is not a valid location. Git gud.</p>}
+      <h3>{location.display_name}</h3>
+      <p>
+        {location.lat} {location.lon}
+      </p>
+      <img src={mapUrl} alt="map" />
     </div>
   );
 }
